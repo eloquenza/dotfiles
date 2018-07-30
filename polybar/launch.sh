9e9d34e53f24ce33081a52822a882cc17cb6602e
Dennis Grabowski <dennisgrabowski@gmx.de>
2018-07-31 01:17:08 +0200

    polybar: rename bar to top

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,5 +8,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -q -r example &
+# Launch top bar
+polybar -q -r top &
